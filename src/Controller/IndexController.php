66b696128308ee30c37c3258faf3cb068bcf00e9
EvgenySKovalenko <valinbox@gmail.com>
2018-10-03 13:52:45 +0300

    Add book form. List of latest books.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,21 +3,56 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Book;
+use App\Entity\Genre;
+use App\Entity\Author;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends AbstractController
 {
-    public function index(AuthorizationCheckerInterface $authChecker)
-    {
-        /*
-        if ($authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-            die('AUTHENTICATED');
-        } else {
-            die('NOT AUTHENTICATED');
-        }
-         */
+    public function index(Request $request)
+    {        
+        //Create Book form
+        $book = new Book();
+        $form_create = $this->createFormBuilder($book)
+            ->add('name', TextType::class)
+            ->add('publishdate', DateType::class)
+            ->add('rating', TextType::class)
+            ->add('genre', EntityType::class, 
+                    ['class' => Genre::class, 'choice_label' => 'name'])
+            ->add('author', EntityType::class, 
+                    ['class' => Author::class, 'choice_label' => 'name'])
+            ->add('save', SubmitType::class, array('label' => 'Add Book'))
+            ->getForm(); 
         
-        return $this->render('index.html.twig', []); 
+        $form_create->handleRequest($request);
+
+        if ($form_create->isSubmitted() && $form_create->isValid()) {           
+            $book_new = $form_create->getData();
+            $now = new \DateTime();
+            $book_new->setDateadded($now);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($book_new);
+            $entityManager->flush();
+        
+            return $this->redirectToRoute('index');
+        }
+        
+        $repository = $this->getDoctrine()->getRepository(Book::class);
+        //$books = $repository->findAll(); 
+        $books = $repository->findBy([], ['dateadded' => 'DESC'], 10);
+        
+        return $this->render('index.html.twig', [
+                'books' => $books,
+                'form_create'  =>  $form_create->createView()
+            ]);        
     }    
+    
+    
 }
 
---
+++ src/Migrations/Version20181003094550.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181003094550 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, genre_id INT NOT NULL, author_id INT NOT NULL, name VARCHAR(255) NOT NULL, publishdate DATE NOT NULL, dateadded DATE NOT NULL, rating DOUBLE PRECISION NOT NULL, INDEX IDX_CBE5A3314296D31F (genre_id), INDEX IDX_CBE5A331F675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A3314296D31F FOREIGN KEY (genre_id) REFERENCES genre (id)');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A331F675F31B FOREIGN KEY (author_id) REFERENCES author (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE book');
+    }
+}
